8f6e398649ca6b1b8215e526f9a2b7b5dad23792
Osybear <osybeargames@gmail.com>
2019-03-16 15:21:11 -0500

    Update
    Basic player controller script
    Simple test world

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    private new Rigidbody rigidbody;
+    [Range(0,100)]
+    public float speed;
+    public TextMeshProUGUI xInputText;
+    public TextMeshProUGUI zInputText;
+    public TextMeshProUGUI velocityText;
+
+    private void Awake() {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical"); 
+        xInputText.text = x.ToString();
+        zInputText.text = z.ToString();
+
+        Vector3 direction = new Vector3(x, 0 ,z);
+        rigidbody.velocity = direction * speed;
+        velocityText.text = rigidbody.velocity.ToString();
+    }   
+}
