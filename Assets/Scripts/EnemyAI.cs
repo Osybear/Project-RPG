6b8f5e5cfbfb8e9c86a6d538f65ab5733aaeedfe
Osybear <osybeargames@gmail.com>
2019-04-20 15:39:09 -0500

    Upadate
    Enemy AI progress

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public LayerMask layerMask;
+    public float radius;
+
+    private void Update() {
+        
+    }
+
+    private GameObject GetNearestPlayer() {
+        Collider[] results = new Collider[25];
+        int hits = Physics.OverlapSphereNonAlloc(transform.position, radius, results, layerMask, QueryTriggerInteraction.UseGlobal);
+
+        for(int i = 0; i < hits; i++) {
+            GameObject player = results[i].transform.gameObject;
+            
+        }
+
+        return nearestEnemy;
+    }
+}
