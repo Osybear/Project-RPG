2a5c2c185a7a5797b4ba28c81c64349936653e8d
Osybear <osybeargames@gmail.com>
2019-05-19 19:43:55 -0500

    Update

--- Assets/Scripts/LocalScript.cs
+++ Assets/Scripts/LocalScript.cs
@@ -6,17 +6,42 @@ public class LocalScript : MonoBehaviour
 {   
     public Animator animator; 
     public new Rigidbody rigidbody;
+    public Plane centerPlane;
     public float speed;
 
-    void Update() {
-        float vertical = Input.GetAxisRaw("Vertical"); 
+    private void Awake() {
+        centerPlane = new Plane(Vector3.up, -1.5f);
+    }
+
+    private void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+            animator.SetTrigger("Swing");
+        }
+        PlayerMovement();
+        LookAtRaycast();
+    }
+
+    private void PlayerMovement() {
         float horizontal = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Vertical", vertical);
-        animator.SetFloat("Horizontal", horizontal);
+        float vertical = Input.GetAxisRaw("Vertical"); 
+        
+        Vector3 globalDirection = new Vector3(horizontal, 0, vertical);
+        rigidbody.velocity = globalDirection * speed;
+        Vector3 localDirection = transform.InverseTransformDirection(globalDirection);
 
-        Vector3 direction = new Vector3(vertical, 0, horizontal);
-        rigidbody.velocity = direction * speed;
-
+        animator.SetFloat("Horizontal", localDirection.x);
+        animator.SetFloat("Vertical", localDirection.z);
+        animator.SetFloat("MotionSpeed", speed);
         animator.SetFloat("Velocity", rigidbody.velocity.magnitude);
+    }
+
+    private void LookAtRaycast() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float enter = 0.0f;
+        if (centerPlane.Raycast(ray, out enter)) {
+            Vector3 hitPoint = ray.GetPoint(enter);
+            hitPoint.y = 1.5f;
+            this.transform.LookAt(hitPoint);
+        }
     }
 }
